1321a69afb8bbedc467ae383e118f326964bec9d
Joshua Nash <joshuangola@gmail.com>
2023-04-03 21:01:24 -0600

    server

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var rollbar = new Rollbar({
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static("public"));
+app.use(express.static(`${__dirname}/public`));
 
 rollbar.log("Welcome to where i log how you interact with me");
 
